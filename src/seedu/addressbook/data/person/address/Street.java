c08e54209f22ddd43864654b16c764a9901ea0b3
amrut-prabhu <amrut08@gmail.com>
2018-02-07 10:27:56 +0800

    Add Street class to address package

---
+++ src/seedu/addressbook/data/person/address/Street.java
@@ -0,0 +1,32 @@
+package seedu.addressbook.data.person.address;
+
+/**
+ * Represents a Person's Street address in the address book.
+ */
+public class Street {
+
+    public final String streetAddress;
+
+    /**
+     * Default constructor
+     */
+    public Street() {
+        this("");
+    }
+
+    /**
+     * Constructs a block with the given block number
+     */
+    public Street(String street) {
+        streetAddress = street;
+    }
+
+    /**
+     * Returns street of the address
+     *
+     * @return the street address of this street
+     */
+    public String getStreet() {
+        return streetAddress;
+    }
+}
